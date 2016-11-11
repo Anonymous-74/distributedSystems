a714595370c9ca3f49308a5a60c332e10c5c062f
faridibindis <farid.adam@st.vvu.edu.gh>
2016-11-11 18:53:56 +0000

    Interface For Telegram Bot

---
+++ dSystems/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta content="IE=edge" http-equiv="X-UA-Compatible">
+	<meta content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no, width=device-width" name="viewport">
+	<title>Project Name</title>
+
+	<!-- css -->
+	<link href="css/base.min.css" rel="stylesheet">
+	<link href="css/project.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<!-- favicon -->
+	<!-- ... -->
+</head>
+<body class="page-brand">
+	<header class="header header-transparent header-waterfall ui-header">
+		<ul class="nav nav-list pull-left">
+			<li>
+				<a data-toggle="menu" href="#ui_menu">
+					<span class="icon icon-lg">menu</span>
+				</a>
+			</li>
+		</ul>
+		<a class="header-logo header-affix-hide margin-left-no margin-right-no" data-offset-top="213" data-spy="affix" href="index.php">Group Name</a>
+	</header>
+	<nav aria-hidden="true" class="menu" id="ui_menu" tabindex="-1">
+		<div class="menu-scroll">
+			<div class="menu-content">
+				<a class="menu-logo" href="index.html">Group Name</a>
+				<ul class="nav">
+					<li>
+						<a class="waves-attach" data-toggle="collapse" href="#appMenu">Level</a>
+						<ul class="menu-collapse collapse in" id="appMenu">
+							<li>
+								<a class="waves-attach" href="#">Sub-Level</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<main class="content">
+		<div class="content-header ui-content-header">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
+						<h1 class="content-heading">Project Name</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2">
+					<section class="content-inner margin-top-no">
+						<div class="card">
+							<div class="card-main">
+								<div class="card-inner">
+									<p>Text Goes Here</p>
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer class="ui-footer">
+		<div class="container">
+			<p id="#gn"><a data-toggle="modal" href="#groupModal">Group Name</a></p>
+		</div>
+	</footer>
+	<div class="fbtn-container">
+		<div class="fbtn-inner">
+			<a class="fbtn fbtn-lg fbtn-brand-accent waves-attach waves-circle waves-light" data-toggle="dropdown">
+				<span class="fbtn-text fbtn-text-left">Tasks</span>
+				<span class="fbtn-ori icon">apps</span>
+				<span class="fbtn-sub icon">close</span></a>
+			<div class="fbtn-dropup">
+				<a class="fbtn waves-attach waves-circle" href="#">
+					<span class="fbtn-text fbtn-text-left"></span>
+					<span class="icon">code</span>
+				</a>
+				<a class="fbtn fbtn-brand waves-attach waves-circle waves-light" href="#">
+					<span class="fbtn-text fbtn-text-left"></span>
+					<span class="icon">code</span>
+				</a>
+				<a class="fbtn fbtn-green waves-attach waves-circle" href="#">
+					<span class="fbtn-text fbtn-text-left"></span>
+					<span class="icon">code</span>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<!--About Group Modal-->
+	<div aria-hidden="true" class="modal fade" id="groupModal" role="dialog" tabindex="-1">
+		<div class="modal-dialog modal-xs">
+			<div class="modal-content">
+				<div class="modal-heading">
+					<a class="modal-close" data-dismiss="modal">×</a>
+					<h2 class="modal-title">Group Name</h2>
+				</div>
+				<div class="modal-inner">
+					<div class="text-center">
+						<p>Asare Nana Nyarko 214CS01001261</p>
+						<p>Kalley Clement 215EI0100</p>
+						<p>Adam Farid 214IT01001738</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- js -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/base.min.js"></script>
+	<script src="js/project.min.js"></script>
+</body>
+</html>
