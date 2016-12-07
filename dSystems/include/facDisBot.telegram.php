256b7548f1e0b4b261e7b38a5c36e3d4e3990598
Nana-Asare <nana.asare@st.vvu.edu.gh>
2016-12-07 02:42:54 +0000

    new update for script

--- dSystems/include/facDisBot.telegram.php
+++ dSystems/include/facDisBot.telegram.php
@@ -1,4 +1,17 @@
 <?php
+	//Start session
+	session_start();
+
+	//Include database connection details
+	include 'db_conn.php';
+	//Include class details
+	include 'core.dSystems.php';
+
+	$obj = new dSystems();
+
+	//$obj->user_dir_list(1);
+
+
 	$botToken="294190711:AAEIGlNiKaglo-1TuGNu2lSKzysUQRToQQo";
 	$website="https://api.telegram.org/bot".$botToken;
 
@@ -15,19 +28,67 @@
 	
 
 	switch ($message) {
-		case '/hi':
-			file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Hey ".$clientName);
-			break;
-		case '/yo':
-			file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Wassup ".$clientName);
-			break;
-		case '/test':
-			file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Test ".$clientName);
-			break;
-		case 'what is your name?':
-		case 'What is your name?':
-			file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=My name is facDisBot");
-			break;
+		case $message:
+			//Create query
+			$query="SELECT * FROM `bot_sense` WHERE `message` = '$message'";
+			$result=mysqli_query($conn, $query);
+			
+			//Check whether the query was successful or not
+			if($result) {
+				if(mysqli_num_rows($result) == 1) {
+					$row = mysqli_fetch_assoc($result);
+					$response = $row['response'];
+
+					file_get_contents($website."/sendmessage?parse_mode=markdown&chat_id=".$chatId."&text=".$response);		
+				}else{
+					switch ($message) {
+						//File Options
+							case '/uploadfile':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/renamefile':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/deletefile':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/movefile':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/copyfile':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/list':
+								
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+
+						
+						//Folder Options
+							case '/newfile':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/renamefolder':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/deletefolder':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/movefolder':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+							case '/copyfolder':
+								file_get_contents($website."/sendmessage?chat_id=".$chatId."&text={$message}");
+								break;
+
+						default:
+							file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=I have not been programmed to understand \"".$message."\" yet");
+							break;
+					}
+				}
+			}
+		break;
+		
 		default:
 			file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=I have not been programmed to understand \"".$message."\" yet");
 			break;
